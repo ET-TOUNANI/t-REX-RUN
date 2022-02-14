d040e52cea346f5184cff870eed77fd0ee5f77d8
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-14 14:43:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    const dino = document.querySelector('dino')
+    
+    const dino = document.querySelector('.dino');
+    let isJumping = false
 
     function control(e){
         if(e.keyCode === 32){
-            console.log('hi')
+            if(!isJumping){
+                isJumping=true
+                jump()
+            }               
         }
+    }
+    document.addEventListener('keyup' ,control)
 
-        document.addEventListener('keyup' ,control)
+    function jump(){
+        position = 0
+        
+        let timer= setInterval(()=>
+        {
+
+            if(position === 150)
+            {
+                clearInterval(timer)
+                let down = setInterval(()=>
+                {
+                    position -=30;
+                    dino.style.bottom = position + 'px'
+
+                    if(position === 0)
+                    {
+                        clearInterval(down)
+                        isJumping=false
+                    }
+
+                },20)
+                
+            }
+            
+            position +=30
+            dino.style.bottom=position +'px'
+
+        },20)
     }
 
 })
